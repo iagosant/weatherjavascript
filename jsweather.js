33ce9f42ec79e3976e9328f2b7f836e859ef2316
Santiago Garza <sgarzaceja@me.com>
2016-03-05 11:37:30 -0500

    need to fix sun x and y trajectory

---
+++ jsweather.js
@@ -0,0 +1,29 @@
+function getWeather() {
+var myRequest = new XMLHttpRequest();
+
+  myRequest.addEventListener('load', function(){
+      var weather = JSON.parse(this.responseText);
+      var sunrise = weather.sys.sunrise;
+      var sunset = weather.sys.sunset;
+      var current_time = new Date()/.getTime()/1000;
+      var seconds_since_sunrise = current_time-sunrise;
+      var percent_left = (seconds_since_sunrise/(sunset-sunrise))*100;
+      var high_noon = (sunset-sunrise)/2;
+
+      if (seconds_since_sunrise < high_noon) {
+          var percent_bottom = (seconds_since_sunrise/high_noon)*100;
+      } else {
+          var percent_bottom = (high_noon/seconds_since_sunrise)
+          // var percent_bottom = 100-()*100;
+      }
+      document.getElementById('sun').style.left = percent_left+'%';
+      document.getElementById('sun').style.bottom = percent_bottom+'%';
+
+      document.getElementById('temperature').innerHTML = parseInt(weather.main.temp)+'&deg;';
+  });
+
+  myRequest.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=Miami+Beach,FL&units=imperial&appid=44db6a862fba0b067b1930da0d769e98')
+  myRequest.send();
+}
+
+window.onload = getWeather;
